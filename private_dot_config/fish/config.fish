7bea4ea8a64332f0096143cd478840d4f7828076
decipher3114 <shauryasiwach26@gmail.com>
2024-06-08 12:17:25 +0530

    enable transient prompt

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,5 +2,9 @@ function fish_greeting
 end
 
 if status is-interactive
+    function starship_transient_prompt_func
+        starship module character
+    end
     starship init fish | source
+    enable_transience
 end
