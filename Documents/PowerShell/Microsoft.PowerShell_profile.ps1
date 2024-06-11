9b8150851047c309be79ba433f2b9cd82b5737aa
decipher3114 <shauryasiwach26@gmail.com>
2024-06-11 16:01:17 +0530

    update prompt

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,7 @@
+function Invoke-Starship-TransientFunction {
+  &starship module character
+}
+
 Invoke-Expression (&starship init powershell)
+
+Enable-TransientPrompt
--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -5,6 +5,9 @@ if status is-interactive
     function starship_transient_prompt_func
         starship module character
     end
+    function starship_transient_rprompt_func
+        starship module time
+    end
     starship init fish | source
     enable_transience
 end
