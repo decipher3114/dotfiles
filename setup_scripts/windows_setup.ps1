363a6335fe3ec99aaa6ea8db7e0a9f35f428add8
decipher3114 <shauryasiwach26@gmail.com>
2025-06-26 20:21:21 +0530

    remove store pkgs

--- setup_scripts/windows_setup.ps1
+++ setup_scripts/windows_setup.ps1
@@ -13,10 +13,4 @@ $wingetPkgs =
     "Google.PlatformTools",
     "clsid2.mpc-hc"
 
-$storePkgs = 
-    "9MZ1SNWT0N5D", # Powershell
-    "9P8LTPGCBZXD", # Wintoys
-    "9N1X3Z50BLM8"  # Visum Photo Viewer
-
 winget install $wingetPkgs -s winget --accept-source-agreements --accept-package-agreements --interactive
-winget install $storePkgs -s msstore --accept-source-agreements --accept-package-agreements --interactive
