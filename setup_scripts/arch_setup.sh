6cf7ba52eedc5164a28a2d8efaa25c0e8a95b5aa
decipher3114 <shauryasiwach26@gmail.com>
2025-08-08 10:34:47 +0530

    fixes

--- setup_scripts/arch_setup.sh
+++ setup_scripts/arch_setup.sh
@@ -14,8 +14,20 @@ if ! grep -q "^ParallelDownloads" "$PACMAN_CONF"; then
     sudo sed -i "/\[options\]/a ParallelDownloads = $TARGET_DOWNLOADS" "$PACMAN_CONF"
 fi
 
-sudo pacman -Syyu
-sudo pacman -S --noconfirm fish starship git
+sudo pacman -Syyu --noconfirm
 
+sudo pacman -S --noconfirm \
+    base-devel \
+    man-db \
+    which \
+    fish \
+    starship \
+    git
 
-chsh -s "$(which fish)"
+FISH_PATH="$(which fish)"
+if ! grep -Fxq "$FISH_PATH" /etc/shells; then
+    echo "$FISH_PATH" | sudo tee -a /etc/shells >/dev/null
+fi
+
+chsh -s "$FISH_PATH"
+
